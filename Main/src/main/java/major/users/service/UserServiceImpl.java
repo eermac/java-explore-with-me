32033d105e36adbc7190f892ae6c2b40d3251ed3
Ermachenkov <ermac.e@yandex.ru>
2023-05-28 23:16:45 +0300

    fix checkstyle

--- Main/src/main/java/major/users/service/UserServiceImpl.java
+++ Main/src/main/java/major/users/service/UserServiceImpl.java
@@ -30,9 +30,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
