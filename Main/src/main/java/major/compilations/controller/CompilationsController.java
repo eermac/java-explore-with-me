41c8333273ee3b4293a2c2a6c5330d405959768f
Ermachenkov <ermac.e@yandex.ru>
2023-05-30 23:10:28 +0300

    checkstyle: fix unused imports and comments

--- Main/src/main/java/major/compilations/controller/CompilationsController.java
+++ Main/src/main/java/major/compilations/controller/CompilationsController.java
@@ -8,7 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 
